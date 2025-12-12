c2b773a5d4261859a1d8a42efacda8bd3875d195
Barsha Lamichhane <barshalamichhane@Barshas-MacBook-Air.local>
2025-12-12 22:22:48 +0100

    fix train, test and frontend

--- src/frontend/organoid-ui/src/app/app.routes.ts
+++ src/frontend/organoid-ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ImageUploadComponent } from './image-upload/image-upload';
+import { ResultComponent } from './results/results'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ImageUploadComponent },
+    { path: 'result', component: ResultComponent },
+    { path: '**', redirectTo: '' }
+];
